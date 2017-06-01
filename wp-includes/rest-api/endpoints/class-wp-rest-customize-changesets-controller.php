bd2769ef5e1822bb3678ee06faf22ab638830b4b
Miina Sikk <miina.sikk@gmail.com>
2017-06-01 09:43:15 +0200

    Fix param name.

--- wp-includes/rest-api/endpoints/class-wp-rest-customize-changesets-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-customize-changesets-controller.php
@@ -66,8 +66,8 @@ class WP_REST_Customize_Changesets_Controller extends WP_REST_Controller {
 	public function ensure_customize_manager( $changeset_uuid = null ) {
 		global $wp_customize;
 		if ( empty( $wp_customize ) || $wp_customize->changeset_uuid() !== $changeset_uuid ) {
-			$settings_preview = false;
-			$wp_customize = new \WP_Customize_Manager( compact( 'changeset_uuid', 'settings_preview' ) ); // WPCS: global override ok.
+			$settings_previewed = false;
+			$wp_customize = new \WP_Customize_Manager( compact( 'changeset_uuid', 'settings_previewed' ) ); // WPCS: global override ok.
 
 			/** This action is documented in wp-includes/class-wp-customize-manager.php */
 			do_action( 'customize_register', $wp_customize );
@@ -1120,8 +1120,8 @@ class WP_REST_Customize_Changesets_Controller extends WP_REST_Controller {
 	 * @return array|null|WP_Post Post object.
 	 */
 	protected function get_customize_changeset_post( $uuid ) {
-		$skip_setting_preview = true;
-		$customize_manager = new WP_Customize_Manager( compact( 'skip_setting_preview' ) );
+		$settings_previewed = true;
+		$customize_manager = new WP_Customize_Manager( compact( 'settings_previewed' ) );
 		$post = get_post( $customize_manager->find_changeset_post_id( $uuid ) );
 
 		return $post;
